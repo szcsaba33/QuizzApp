683c160b796e70638cf540850c34cbd49fbde188
N2505 <n2505@student.jamk.fi>
2019-11-11 14:21:00 +0200

    Task

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><b>Quiz App</b></b-nav-item>
+            <b-nav-item disabled>Counter: {{numCorrect}}/{{numTotal}} </b-nav-item>
+            <b-nav-item disabled>Number of Questions: {{amount}}</b-nav-item>
+            <li v-for="i in optionsCat" v-bind:key="{ value: i.value, text: i.text }">
+                <b-nav-item disabled v-if="i.value === selectedCategory">Selected Category: {{i.text}}</b-nav-item>
+            </li>
+            <li disabled v-for="i in optionsDiff" v-bind:key="{ value: i.value, text: i.text }">
+                <b-nav-item disabled v-if="i.value === selectedDifficulty">Selected Difficulty: {{i.text}}</b-nav-item >
+            </li>
+            <li v-for="i in optionsType" v-bind:key="{ value: i.value, text: i.text }">
+                <b-nav-item disabled v-if="i.value === selectedType">Selected Type: {{i.text}}</b-nav-item>
+            </li>
+
+
+        </b-nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'numCorrect',
+            'numTotal',
+            'amount',
+            'selectedCategory',
+            'selectedDifficulty',
+            'selectedType',
+            'optionsCat',
+            'optionsDiff',
+            'optionsType'
+        ],
+
+    }
+</script>
